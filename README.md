d84615ea0b416c5fbe2b1a59e38f21d23f0f1578
Pachá <pacha@pop-os.localdomain>
2019-10-06 20:04:50 -0300

    1st listing for marketplace

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rstudio-server-pkg-dev-workshop-droplet
+# Application description
+
+RStudio Server is the premier integrated development environment for R that allows to move computation close to the data and scale compute and RAM centrally.
+
+See the details and available software at [Digital Ocean Marketplace](https://marketplace.digitalocean.com/apps/rstudio).
---
+++ scripts/01-packages.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Add a swap file to prevent build time OOM errors
+fallocate -l 8G /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+# add CRAN to apt sources
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+printf '\n#CRAN mirror\ndeb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/\n' | sudo tee -a /etc/apt/sources.list
+
+# update apt
+apt-get -y update
+apt-get -y upgrade
+
+# requisites for RStudio
+apt-get -y install gdebi-core
+
+# requisites for R packages
+apt-get -y install libxml2-dev libssl-dev libcurl4-openssl-dev
+
+# install R
+apt-get -y install r-base r-base-dev
+
+# install Digital Ocean agent
+curl -sSL https://repos.insights.digitalocean.com/install.sh | sudo bash
+
+# install R packages
+R --vanilla << EOF
+if (!require("rmarkdown")) {
+  install.packages("rmarkdown", repos = "http://cran.us.r-project.org")
+}
+
+if (!require("devtools")) {
+  install.packages("devtools", repos = "http://cran.us.r-project.org")
+}
+
+if (!require("roxygen2")) {
+  install.packages("roxygen2", repos = "http://cran.us.r-project.org")
+}
+
+if (!require("testthat")) {
+  install.packages("testthat", repos = "http://cran.us.r-project.org")
+}
+
+if (!require("knitr")) {
+  install.packages("knitr", repos = "http://cran.us.r-project.org")
+}
+
+if (!require("covr")) {
+  install.packages("covr", repos = "http://cran.us.r-project.org")
+}
+q()
+EOF
+
+# install RStudio Server
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.2.5001-amd64.deb
+gdebi --n rstudio-server-1.2.5001-amd64.deb
+rm rstudio-server-1.2.5001-amd64.deb
+
+# open ports
+ufw allow http
+ufw allow https
+ufw allow ssh
+ufw allow 8787
+
+# Disable and remove the swapfile prior to snapshotting
+swapoff /swapfile
+rm -f /swapfile
